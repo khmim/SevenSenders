cc61b36a1025355c54e00374c0b7cf1866c68441
maryam <maryamkhandaghabadi@gmail.com>
2022-05-04 16:38:11 +0430

    refactor

--- src/main/java/com/SevenSenders/demo/controller/StripController.java
+++ src/main/java/com/SevenSenders/demo/controller/StripController.java
@@ -3,26 +3,17 @@ package com.SevenSenders.demo.controller;
 import com.SevenSenders.demo.service.domain.StripDto;
 import com.SevenSenders.demo.service.domain.StripService;
 import com.SevenSenders.demo.service.domain.StripsDto;
-import com.rometools.rome.feed.atom.*;
-import com.rometools.rome.feed.rss.Channel;
-import com.rometools.rome.feed.rss.Description;
-import com.rometools.rome.feed.rss.Image;
-import com.rometools.rome.feed.rss.Item;
-import com.rometools.rome.feed.synd.*;
 import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.SyndFeedOutput;
-import com.rometools.rome.io.XmlReader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Slf4j
 @RequiredArgsConstructor
@@ -36,38 +27,16 @@ public class StripController {
     private StripsDto stripsDto= new StripsDto();
 
 
-    @GetMapping("/start")
+    @GetMapping("/")
     @ResponseStatus(HttpStatus.OK)
-    public StripsDto getLaststrip() {
-        String stripUri = "https://xkcd.com/info.0.json";
-        RestTemplate restTemplate = new RestTemplate();
-        StripDto strip = restTemplate.getForObject(stripUri, StripDto.class);
-        StripsDto stripsDto=getLastStrips(strip.getNum());
+    public StripsDto getLastComics() throws FeedException, IOException {
+        List<StripDto> strips = new ArrayList<StripDto>();
+        strips.addAll((stripService.getFeed().getStrips()));
+        strips.addAll(stripService.getLaststrip().getStrips());
+        stripsDto.setStrips(strips);
         return stripsDto;
     }
 
-    @GetMapping("/")
-    @ResponseStatus(HttpStatus.OK)
-    public StripsDto getLastStrips(@PathVariable int stripNum){
-        List<StripDto> strips = new ArrayList<>();
-        for(int i =0 ; i<9;i++){
-            stripNum--;
-            String stripUri = "https://xkcd.com/{stripNum}/info.0.json";
-            Map<String, String> stripParams = new HashMap<String, String>();
-            stripParams.put("stripNum", String.valueOf(stripNum));
-            RestTemplate restTemplate = new RestTemplate();
-            StripDto strip = restTemplate.getForObject(stripUri, StripDto.class, stripParams);
-            strips.add(strip);
-        }
-        stripsDto.setStrips(strips);
-        return stripService.sortingByDate(stripsDto);
-    }
 
-    @GetMapping(path = "/rss")
-    @ResponseBody
-    public String getFeed () throws FeedException, IOException {
-        SyndFeedInput input = new SyndFeedInput();
-        SyndFeed feed = input.build(new XmlReader(new URL("http://feeds.feedburner.com/PoorlyDrawnLines")));
-        return new SyndFeedOutput().outputString(feed);
-    }
+
 }
--- src/main/java/com/SevenSenders/demo/service/domain/StripService.java
+++ src/main/java/com/SevenSenders/demo/service/domain/StripService.java
@@ -1,18 +1,67 @@
 package com.SevenSenders.demo.service.domain;
 
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 @Service
 public class StripService {
+
+    private StripsDto stripsDto= new StripsDto();
 
     public StripsDto sortingByDate (StripsDto stripsDto){
         List<StripDto> strips = stripsDto.getStrips();
         Collections.sort(strips,StripDto.Comparators.Year);
         stripsDto.setStrips(strips);
+        return stripsDto;
+    }
+
+    public StripsDto getFeed () throws FeedException, IOException {
+        List<StripDto> strips = new ArrayList<>();
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(new URL("http://feeds.feedburner.com/PoorlyDrawnLines")));
+        System.out.println(feed);
+        List<SyndEntry> comics= feed.getEntries();
+        for(int i =0 ; i<9;i++){
+            StripDto strip= new StripDto();
+            strip.setYear(comics.get(i).getPublishedDate().toString());
+            strip.setImg(comics.get(i).getUri());
+            strip.setLink(comics.get(i).getLink());
+            strip.setTitle(comics.get(i).getTitle());
+            strips.add(strip);
+        }
+        stripsDto.setStrips(strips);
+        return sortingByDate(stripsDto);
+    }
+
+    public StripsDto getLastStrips(@PathVariable int stripNum){
+        List<StripDto> strips = new ArrayList<>();
+        for(int i =0 ; i<9;i++){
+            stripNum--;
+            String stripUri = "https://xkcd.com/{stripNum}/info.0.json";
+            Map<String, String> stripParams = new HashMap<String, String>();
+            stripParams.put("stripNum", String.valueOf(stripNum));
+            RestTemplate restTemplate = new RestTemplate();
+            StripDto strip = restTemplate.getForObject(stripUri, StripDto.class, stripParams);
+            strips.add(strip);
+        }
+        stripsDto.setStrips(strips);
+        return sortingByDate(stripsDto);
+    }
+    public StripsDto getLaststrip() {
+        String stripUri = "https://xkcd.com/info.0.json";
+        RestTemplate restTemplate = new RestTemplate();
+        StripDto strip = restTemplate.getForObject(stripUri, StripDto.class);
+        StripsDto stripsDto=getLastStrips(strip.getNum());
         return stripsDto;
     }
 }
